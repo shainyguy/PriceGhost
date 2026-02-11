ef64bdbac140586dfbf99fbf460f9b4536811c45
shainyguy <shainyguy@gmail.com>
2026-02-11 10:10:04 +0300

    Update init.py

--- database/init.py
+++ database/init.py
@@ -1,7 +1,1 @@
-from database.db import Database, get_db
-from database.models import Base, User, Product, PriceRecord, MonitoredProduct, Payment
 
-__all__ = [
-    "Database", "get_db",
-    "Base", "User", "Product", "PriceRecord", "MonitoredProduct", "Payment"
-]
